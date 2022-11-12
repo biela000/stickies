1adbe6b995f368ff6c4d96f3c10fb2538622a2cb
biela000 <jak.biela000@gmail.com>
2022-11-12 20:00:05 +0100

    Change testing variable

--- src/ts/board.ts
+++ src/ts/board.ts
@@ -3,5 +3,5 @@ import Board from "./classes/Board";
 
 const boardName: string = window.location.pathname.split('/').pop();
 
-const board = new Board('czerwonieak', ".stck-brd", ".n-note");
+const board = new Board(boardName, ".stck-brd", ".n-note");
 board.start();
