54182ea460fbdf31da65d85c145c853e6de99f5f
biela000 <jak.biela000@gmail.com>
2022-11-22 23:00:57 +0100

    Hotfix

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,4 @@
+import "../scss/index.scss";
 import FormUtils from "./classes/FormUtils";
 
 FormUtils.addSubmitEventHandler(document.querySelector('.board-name-form'), document.querySelector('input[name=\"board-name\"]'));
