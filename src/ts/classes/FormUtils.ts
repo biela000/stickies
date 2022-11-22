a349637a2d0d1e745c42fc73344fc322e88dedbd
biela000 <jak.biela000@gmail.com>
2022-11-22 22:51:45 +0100

    Style index page, secure form

--- src/ts/classes/FormUtils.ts
+++ src/ts/classes/FormUtils.ts
@@ -2,8 +2,10 @@ class FormUtils {
    public static addSubmitEventHandler(form: HTMLFormElement, nameInput: HTMLInputElement) {
        form.addEventListener('submit', async (e) => {
            e.preventDefault();
-           window.location.replace(`${window.location.protocol}//${window.location.host}/boards/${nameInput.value.trim()}`)
+	   if (nameInput.value.trim().length >= 5 && nameInput.value.trim().length <= 20) {
+           	window.location.replace(`${window.location.protocol}//${window.location.host}/boards/${nameInput.value.trim()}`)
            //window.location.replace(`${window.location.protocol}//${window.location.host}/dist/sticky-page.html`)
+	   }
        });
    }
 }
